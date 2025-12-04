45df676b1a4255a12f04e77338f162016ff4c8d1
Vishal Gunjal <vishalgunjal0287@gmail.com>
2025-12-04 13:19:05 +0530

    Add outputs.tf

---
+++ terraform_OS/outputs.tf
@@ -0,0 +1,33 @@
+# output "ec2_public_ip" {
+#   value = aws_instance.my_instance[*].public_ip
+# }
+
+# output "ec2_public_dns" {
+#   value = aws_instance.my_instance[*].public_dns
+# }
+
+# output "ec2_private_ip" {
+#   value = aws_instance.my_instance[].private_ip
+# }
+
+
+output "ec2_public_ip" {
+    value = [
+        for instance in aws_instance.my_instance : instance.public_ip
+    ]
+  
+}
+
+output "ec2_public_dns" {
+    value = [
+        for instance in aws_instance.my_instance : instance.public_dns
+    ]
+  
+}
+
+output "ec2_private_ip" {
+    value = [
+        for instance in aws_instance.my_instance : instance.private_ip
+    ]
+  
+}
